b67c35d6159bdddd390cc38f38e0d03833cb3073
gaetanbrl <gaetan.bruel@jdev.fr>
2024-02-21 12:14:13 +0100

    fix spaces and allow to force create file

--- start.sh
+++ start.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 echo "PROCESS.....START"
 
+refresh_file="$1"
+
 env_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # Create env var
@@ -8,21 +10,30 @@ env_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 export GGl_PLACES_CSV="$env_dir/place_hours.csv"
 export GGl_PLACES_JSON="$env_dir/places.json"
 
-export GGL_API_KEY = ""
-export EDP_GEOSERVER_USER = ""
-export EDP_GEOSERVER_PASSWORD = ""
-export EDP_GEOSERVER_URL = ""
-export EDP_COMMERCE_LAYER = ""
+export GGL_API_KEY=""
+export EDP_GEOSERVER_USER=""
+export EDP_GEOSERVER_PASSWORD=""
+export EDP_GEOSERVER_URL=""
+export EDP_COMMERCE_LAYER=""
 
-export EDP_DB_NAME = ""
-export EDP_DB_HOST = ""
-export EDP_DB_USER = ""
-export EDP_DB_PASSWORD = ""
+export EDP_DB_NAME=""
+export EDP_DB_HOST=""
+export EDP_DB_USER=""
+export EDP_DB_PASSWORD=""
 
 install_script="$env_dir/script/python/install.sh"
 ogr_script="$env_dir/script/ogr/set_hours.sh"
 venv_path="$env_dir/script/python/.venv"
 process_file="$env_dir/script/python/main.py"
+
+json_file_place="$env_dir/places.json"
+
+if [ $refresh_file = "true" ] && [ -f "$json_file_place" ]
+then
+    rm $json_file_place
+else
+    echo "KEEP PLACES FILE : $json_file_place"
+fi
 
 # install
 . "$install_script"
@@ -30,7 +41,9 @@ process_file="$env_dir/script/python/main.py"
 . "$venv_path/bin/activate"
 python "$process_file"
 deactivate
+
 echo "CREATE OR UPDATE LAYER....."
 # load postgis table
 . "$ogr_script"
+
 echo "PROCESS.....SUCCESS !"
